dc1fa84e9aa43ce8a1020599efca49cfbe2ef32b
anas <civil.anas1000@gmail.com>
2021-06-14 00:16:07 +0300

    pass all challenges

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
@@ -22,9 +22,8 @@ For example:
 Returns: ['dyoll', 'eimaj'];
 ------------------------------------------------------------------------------------------------ */
 
-const getNames = (arr) => arr.map(people=>people.name.split("").reverse().join(""));
-
-
+const getNames = (arr) =>
+  arr.map((people) => people.name.split("").reverse().join(""));
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 2
@@ -34,7 +33,8 @@ Write a function that appends ' The end.' to a string, and returns the modified 
 ------------------------------------------------------------------------------------------------ */
 
 const appendTheEnd = (str) => {
-  // Solution code here...
+  let newStrain = str;
+  return `${newStrain} The end.`;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -51,7 +51,8 @@ console.log(a) prints [1, 2, 3, 1]
 ------------------------------------------------------------------------------------------------ */
 
 const appendFirstToLast = (arr) => {
-  // Solution code here...
+  let newArrat = arr;
+  newArrat.push(arr[0]);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -70,7 +71,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 ------------------------------------------------------------------------------------------------ */
 
 const addBirthYearProperty = (obj, year) => {
-  // Solution code here...
+  obj.yearBorn = year;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -107,7 +108,6 @@ console.log(a) prints [1, 2, 3, 4]
 
 const append = (arr1, arr2) => {
   // Solution code here...
-
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -121,44 +121,53 @@ Run your tests from the console: jest challenges-02.test.js
 
 ------------------------------------------------------------------------------------------------ */
 
-describe('Testing challenge 1', () => {
-  test('It returns an array of names reversed', () => {
-    expect(getNames([{name:'lloyd', age: 32, shoeSize: 12}, {name:'jamie', age:21, shoeSize: 8}])).toStrictEqual(['dyoll', 'eimaj']);
+describe("Testing challenge 1", () => {
+  test("It returns an array of names reversed", () => {
+    expect(
+      getNames([
+        { name: "lloyd", age: 32, shoeSize: 12 },
+        { name: "jamie", age: 21, shoeSize: 8 },
+      ])
+    ).toStrictEqual(["dyoll", "eimaj"]);
     expect(getNames([])).toStrictEqual([]);
   });
 });
 
-describe('Testing challenge 2', () => {
-  test('It should append without modifying the oiginal', () => {
-    const a = 'This is my story.';
+describe("Testing challenge 2", () => {
+  test("It should append without modifying the oiginal", () => {
+    const a = "This is my story.";
     const b = appendTheEnd(a);
 
-    expect(a).toStrictEqual('This is my story.');
-    expect(b).toStrictEqual('This is my story. The end.');
+    expect(a).toStrictEqual("This is my story.");
+    expect(b).toStrictEqual("This is my story. The end.");
   });
 });
 
-describe('Testing challenge 3', () => {
-  test('It should append by modifying the oiginal', () => {
-    const a = ['Yes', 'it', 'is'];
+describe("Testing challenge 3", () => {
+  test("It should append by modifying the oiginal", () => {
+    const a = ["Yes", "it", "is"];
     appendFirstToLast(a);
 
-    expect(a).toStrictEqual(['Yes', 'it', 'is', 'Yes']);
+    expect(a).toStrictEqual(["Yes", "it", "is", "Yes"]);
   });
 });
 
-describe('Testing challenge 4', () => {
-  test('It should add a property to an object', () => {
-    const a = { fullName: 'Octavia Butler' };
+describe("Testing challenge 4", () => {
+  test("It should add a property to an object", () => {
+    const a = { fullName: "Octavia Butler" };
     addBirthYearProperty(a, 1947);
 
     expect(a.yearBorn).toStrictEqual(1947);
   });
 });
 
-xdescribe('Testing challenge 5', () => {
-  test('It should add a property to every object in an array', () => {
-    const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
+xdescribe("Testing challenge 5", () => {
+  test("It should add a property to every object in an array", () => {
+    const a = [
+      { fullName: "Octavia Butler" },
+      { fullName: "Ray Bradbury" },
+      { fullName: "Kurt Vonnegut" },
+    ];
     setStatusAsAuthor(a);
 
     expect(a[0].isAuthor).toStrictEqual(true);
@@ -167,8 +176,8 @@ xdescribe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
-  test('It should append the second array to the first', () => {
+xdescribe("Testing challenge 6", () => {
+  test("It should append the second array to the first", () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
     append(a, b);
