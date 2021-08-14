3c14ad543eebbdf051fb61b65055067b04541ce3
anas <civil.anas1000@gmail.com>
2021-08-14 21:20:49 +0300

    adding tests

--- python/code_challenges/tree_fizz_buzz/tests/test_tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz/tests/test_tree_fizz_buzz.py
@@ -1,5 +1,51 @@
+import pytest
 from tree_fizz_buzz import __version__
-
-
+from tree_fizz_buzz.tree_fizz_buzz import *
 def test_version():
     assert __version__ == '0.1.0'
+
+@pytest.fixture
+def data():
+
+    tree = k_ary_tree()
+    tree.root = Node(3)
+
+    tree.root.children.append(Node(2))
+    tree.root.children.append(Node(5))
+    tree.root.children.append(Node(15))
+
+    tree.root.children[0].children.append(Node(4))
+    tree.root.children[0].children.append(Node(6))
+    tree.root.children[0].children.append(Node(7))
+
+    tree.root.children[1].children.append(Node(9))
+    tree.root.children[1].children.append(Node(18))
+    tree.root.children[2].children.append(Node(45))
+    
+
+    return tree
+
+
+def test_if_fizz(data):
+    new_tree =  fizz_buzz_tree(data)
+    actual=new_tree.root.value
+    expected='Fizz'
+    assert actual==expected
+
+def test_if_Buzz(data):
+    new_tree =  fizz_buzz_tree(data)
+    actual=new_tree.root.children[1].value
+    expected='Buzz'
+    assert actual==expected
+
+def test_if_fizzBuzz(data):
+    new_tree =  fizz_buzz_tree(data)
+    actual=new_tree.root.children[2].value
+    expected='FizzBuzz'
+    assert actual==expected
+
+def test_raise():
+    x=None
+    with pytest.raises(ValueError, match="Empty Root"):
+     new_tree =  fizz_buzz_tree(x)
+      
--- python/code_challenges/tree_fizz_buzz/tree_fizz_buzz/tree_fizz_buzz.py
+++ python/code_challenges/tree_fizz_buzz/tree_fizz_buzz/tree_fizz_buzz.py
@@ -5,45 +5,132 @@ class Node:
         self.value = value
         self.children = []
 
-class k_arytree:
+
+class Queue:
+    def __init__(self):
+        self.front=None
+        self.rear=None
+    def enqueue(self,value):
+      
+        if self.is_empty():
+            self.front=value
+            self.rear=value
+        self.rear.next=value
+        self.rear=value
+
+    def dequeue(self):
+        if self.is_empty():
+            raise Exception("empty equeue")
+        if self.front==self.rear:
+            temp=self.front
+            self.front=None
+            self.rear=None
+            return temp.value
+        else:
+            temp=self.front
+            self.front=self.front.next
+            temp.next=None
+            return temp.value
+
+    def peek(self):
+       if self.is_empty():
+           raise Exception("empty equeue")
+       return self.front.value
+
+
+    def is_empty(self):
+     return not self.front
+
+    def __len__(self):
+        counter=0
+        while self.front:
+            counter +=1
+            self.dequeue()
+        return counter
+
+
+class k_ary_tree:
     def __init__(self):
         self.root = None
 
+
 def fizz_buzz_tree(k_array):
-    '''this function check the value of the trees and return fizz , buzz or fizz buzz based on the value  '''
-   
-    if not k_array.root:
-        return None
+   try:
+     
+        copy_root=copy.deepcopy(k_array)
+        temp=Queue()
+        temp.enqueue(copy_root.root)
+        print(temp.front.value)
 
-    new_tree = copy.deepcopy(k_array)
-    allnodes = []
-    allnodes.append(new_tree.root)
-    val = allnodes[0]
-    
-    while True:
-        
-        if val.children:
-            allnodes += val.children
+        while temp.front:
+
+            [temp.enqueue(i) for i in temp.front.children if temp.front.children]
+
+            if (temp.front.value % 3 == 0 and temp.front.value % 5 == 0):
+                temp.front.value='FizzBuzz'
+
+            elif temp.front.value % 3 == 0:
+                temp.front.value='Fizz'
+
+            elif temp.front.value % 5 == 0:
+                temp.front.value='Buzz'   
+                
+            temp.dequeue()
             
-        if val.value % 3 == 0 and val.value % 5 == 0:
-            val.value = 'FizzBuzz'
+        return copy_root
+        
+   except:
+        raise ValueError('Empty Root')
 
-        elif val.value % 3 == 0:
-            val.value = 'Fizz'
-
-        elif val.value % 5 == 0:
-            val.value = 'Buzz'
-
-        else:
-            val.value = str(val.value)
-
-        allnodes.pop(0)
-
-        if not allnodes:
-            break
-
-        val = allnodes[0]
-
-    return new_tree
+     
 
 
+
+if __name__ == "__main__":
+    
+    tree = k_ary_tree()
+    tree.root = Node(3)
+
+    tree.root.children.append(Node(2))
+    tree.root.children.append(Node(5))
+    tree.root.children.append(Node(15))
+
+    tree.root.children[0].children.append(Node(4))
+    tree.root.children[0].children.append(Node(6))
+    tree.root.children[0].children.append(Node(7))
+
+    tree.root.children[1].children.append(Node(9))
+    tree.root.children[1].children.append(Node(18))
+    tree.root.children[2].children.append(Node(45))
+    new_tree =  fizz_buzz_tree(tree)
+
+    # print(new_tree.root.children[1].children[1].value)
+    print(new_tree.root.value)
+
+    # print(dir([]))
+    # a = new_tree.__init__()
+    # print(a)
+
+
+#               Input (k_arr_tree)
+
+
+#                      3 
+#                ______|______
+#               /      |      \
+#              /       |       \
+#             2        5        15
+#           / | \     / \        \
+#          4  6  7   9   18       45 
+#                   Output 
+
+
+#                      F
+#                ______|______
+#               /      |      \
+#              /       |       \
+#             2        B        FB
+#           / | \     / \        \
+#          4  F  7   F   F       FB 
+
+#     FB =>  FizzBuzz F => Fizz , B => Buzz
